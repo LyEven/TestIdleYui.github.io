164cb7e22cdcac17da82b104d9b5e2550a0c990a
LyEven <81610243+LyEven@users.noreply.github.com>
2026-01-31 20:09:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let points = 0;
+
+const pointsDisplay = document.getElementById("points");
+const clickBtn = document.getElementById("clickBtn");
+
+clickBtn.addEventListener("click", () => {
+    points++;
+    pointsDisplay.textContent = points;
+});
+
+let pointsParSeconde = 1;
+
+setInterval(() => {
+    points += pointsParSeconde;
+    pointsDisplay.textContent = points;
+}, 1000);
+// Charger la sauvegarde
+const savedPoints = localStorage.getItem("points");
+if (savedPoints) {
+    points = parseInt(savedPoints);
+    pointsDisplay.textContent = points;
+}
+
+// Sauvegarde automatique
+setInterval(() => {
+    localStorage.setItem("points", points);
+}, 3000);
